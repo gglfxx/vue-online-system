acc7e227bfedfa896e43dc564033bdb7d4835cb4
guiguoliang <584961991@qq.com>
2021-02-02 13:09:51 +0800

    https://github.com/gglfxx/vue-online-system

---
+++ src/components/home.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>你在说什么</div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Cookies from 'js-cookie'
+import Element from 'element-ui'
+import './styles/element-variables.scss'
 
 Vue.config.productionTip = false
 
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+})
