32cc5ae9e4f805d5b5c49cbc8c6946385f0361f5
guiguoliang <584961991@qq.com>
2021-02-05 16:57:18 +0800

    修改app样式

--- src/components/home.vue
+++ src/components/home.vue
@@ -8,10 +8,15 @@
       </div>
     </div>
     <div class="banner">
-      <img src="../assets/avengers.png" />
-    </div>
-    <div class="movies">
-      <div class="movie-index" v-for="(value,index) in imageList"  v-bind:key="index"></div>
+      <el-carousel :interval="4000" indicator-position="outside" height="195px" ref="carousel">
+      <el-carousel-item v-for="(item,index) in banners" :key="item.url">
+        <v-touch :swipe-options="{direction: 'horizontal'}" v-on:swipeleft="swiperleft(index)" v-on:swiperight="swiperright(index)" class="wrapper">
+        <div class="menu-container" ref="menuContainer">   
+          <img v-lazy="item.url"/>
+        </div>
+       </v-touch>
+      </el-carousel-item>
+    </el-carousel>
     </div>
     <div class="main-nav">
       <ul>
@@ -23,25 +28,25 @@
     </div>
     <div class="main-lastet-broad">
       <h4>正在热播</h4>
-      <ul>
-        <li><a href="#"><span>热门</span></a></li>
-        <li><a href="#"><span>预告</span></a></li>
-        <li><a href="#"><span>榜单</span></a></li>
-        <li><a href="#"><span>榜单</span></a></li>
-        <li><a href="#"><span>榜单</span></a></li>
-        <li><a href="#"><span>榜单</span></a></li>
-        <li><a href="#"><span>榜单</span></a></li>
-        <li><a href="#"><span>榜单</span></a></li>
-        <li><a href="#"><span>榜单</span></a></li>
-      </ul>
+      <v-touch :swipe-options="{direction: 'horizontal'}" v-on:swipeleft="left" v-on:swiperight="right" class="wrapper">
+        <ul>
+          <li v-for="(item) in hotImages" :key="item.url">
+            <img v-lazy="item.url"/>
+            <span>{{item.title}}</span>
+          </li>
+        </ul>
+       </v-touch>
     </div>
     <div class="main-lastet-online">
       <h4>最新上线</h4>
-      <ul>
-        <li><a href="#"><span>热门</span></a></li>
-        <li><a href="#"><span>预告</span></a></li>
-        <li><a href="#"><span>榜单</span></a></li>
-      </ul>
+      <v-touch :swipe-options="{direction: 'horizontal'}" v-on:swipeleft="left" v-on:swiperight="right" class="wrapper">
+        <ul>
+          <li v-for="(item) in hotImages" :key="item.url">
+            <img v-lazy="item.url"/>
+            <span>{{item.title}}</span>
+          </li>
+        </ul>
+       </v-touch>
     </div>
     <div class="bottom">
       <router-view></router-view>
@@ -89,30 +94,16 @@
 .banner {
   margin: 0 auto;
   width: 95%;
-  height: 12rem;
+  height: 13rem;
 }
 .banner img{
     width: 100%;
     height: 100%;
     border-radius: 2rem;
-    box-shadow: 0.5rem 0.5rem 0.5rem #ebebeb;
-}
-.movies {
-  width: 95%;
-  display: flex;
-  justify-content: center;
-  position: absolute;
-  margin-top: 0.5rem;
-  align-items: center;
-}
-.movie-index {
-  width: 10px;
-  margin-left: 5px;
-  border: 1px solid #26231f;
 }
 .main-nav{
   width:100%;
-  margin:1.5rem auto;
+  margin:0 auto;
 }
 .main-nav ul{
     position: relative;
@@ -134,6 +125,9 @@
   margin-left: 0.6rem;
   font-family: sanfranciscotext;
 }
+.wrapper {
+  touch-action: pan-y !important;
+}
 .main-lastet-broad,
 .main-lastet-online {
   width: 95%;
@@ -144,20 +138,24 @@
 .main-lastet-online h4{
   height: 0.5rem;
 }
-.main-lastet-broad ul{
-  position: relative;
+.main-lastet-broad ul,.main-lastet-online ul{
   white-space: nowrap;
-  overflow-y: auto;
+  overflow-x: auto;
+  padding: 0;
   display: flex;
 }
-.main-lastet-broad ul li{
-  border-radius: 1rem;
+.main-lastet-broad ul li,.main-lastet-online ul li{
+  position: relative;
   display: inline-block;
   height: 8rem;
-  width: 6rem;
-  border: 1px solid #f5f5f5;
+  margin-right: 1rem;
 }
-.main-lastet-broad ul li span{
+.main-lastet-broad ul li img,.main-lastet-online ul li img{
+    height: 100%;
+    width:5.3rem;
+    border-radius: 0.5rem;
+}
+.main-lastet-broad ul li span,.main-lastet-online ul li span{
   position:absolute;
   bottom:0;
   font-size: 0.8rem;
@@ -186,8 +184,39 @@ export default {
         {url:"",name:"1"},
         {url:"",name:"2"},
         {url:"",name:"3"},
+      ],
+      banners:[
+        {url:require("../assets/avengers.png"),title:"复仇者联盟"},
+        {url:require("../assets/furys.png"),title:"狂怒1"},
+        {url:require("../assets/wolf.png"),title:"狂怒2"}
+      ],
+      hotImages:[
+        {url:require("../assets/avengers.png"),title:"复仇者联盟"},
+        {url:require("../assets/furys.png"),title:"狂怒1"},
+        {url:require("../assets/wolf.png"),title:"狂怒2"}
       ]
     };
   },
+  methods: {
+    //设置滑动切换轮播图
+    swiperleft: function (index) {
+      //上一页
+      this.$refs.carousel.prev();
+      //设置幻灯片的索引
+      this.$refs.carousel.setActiveItem(index - 1);
+    },
+    swiperright: function (index) {
+      //下一页
+      this.$refs.carousel.next();
+      this.$refs.carousel.setActiveItem(index + 1);
+    },
+    //左右滑动
+    left:function(){
+      console.log("左划")
+    },
+    right:function(){
+
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import Cookies from 'js-cookie'
 import Element from 'element-ui'
+import VueTouch from 'vue-touch'
+import VueLazyload from 'vue-lazyload'
 import './styles/element-variables.scss'
 import '@/styles/font.scss';
 
@@ -11,7 +13,12 @@ Vue.config.productionTip = false
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
 })
-
+Vue.use(VueTouch, {name: 'v-touch'})
+VueTouch.config.swipe = {
+  threshold: 100 //手指左右滑动距离
+}
+// options 为可选参数，无则不传
+Vue.use(VueLazyload);
 new Vue({
   el: '#app',
   router,
